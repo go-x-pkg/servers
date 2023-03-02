5ac0e1ba7da41560cee4973723625333a332725e
Федорченко Олег Васильевич <OVFedorchenko@sberbank.ru>
2023-03-02 12:13:56 +0300

    restore args.go

--- args.go
+++ args.go
@@ -7,18 +7,12 @@ import (
 	"github.com/go-x-pkg/log"
 )
 
-type logGRPC struct {
-	logger   log.FnT
-	levelMin log.Level
-}
 type args struct {
 	fnShutdownTimeout func() time.Duration
 
 	fnLog log.FnT
 
 	fnLogHTTPError log.FnT
-
-	logGRPC
 
 	ctx context.Context
 }
@@ -27,8 +21,6 @@ func (cfg *args) defaultize() {
 	cfg.fnShutdownTimeout = defaultFnShutdownTimeout
 	cfg.fnLog = defaultFnLog
 	cfg.fnLogHTTPError = defaultFnLog
-	cfg.logGRPC.logger = defaultFnLog
-	cfg.logGRPC.levelMin = log.Quiet
 	cfg.ctx = context.TODO()
 }
 
@@ -44,13 +36,6 @@ func FnLog(v log.FnT) Arg {
 
 func FnLogHTTPError(v log.FnT) Arg {
 	return func(cfg *args) { cfg.fnLogHTTPError = v }
-}
-
-func LogGRPC(logger log.FnT, levelMin log.Level) Arg {
-	return func(cfg *args) {
-		cfg.logGRPC.logger = logger
-		cfg.logGRPC.levelMin = levelMin
-	}
 }
 
 func Context(v context.Context) Arg {
