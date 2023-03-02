f579ec0956d939f11d234a76affe6e51a1982fc7
Федорченко Олег Васильевич <OVFedorchenko@sberbank.ru>
2023-03-02 10:31:44 +0300

    fix tlsAuthConfig trustedCa field yaml name

--- client-auth-tls-config.go
+++ client-auth-tls-config.go
@@ -37,7 +37,7 @@ type ClientAuthTLSConfig struct {
 	// If ClientAuthTLS is set true, AuthType must be set.
 	AuthType clientAuthTypeTLS `json:"authType" yaml:"authType" bson:"authType"`
 	// CARoot certificate for clients certificates. Optional.
-	TrustedCA string `json:"caCert" yaml:"trustedCa" bson:"caCert"`
+	TrustedCA string `json:"caCert" yaml:"caCert" bson:"caCert"`
 	// If set, server will verifie Common Name of certificate given by client has in this list.
 	// Otherwise server return Unauthtorized response.
 	ClientCommonNames []string `json:"clientCommonNames" yaml:"clientCommonNames" bson:"clientCommonNames"`
@@ -54,7 +54,7 @@ func (c *ClientAuthTLSConfig) dump(ctx *dumpctx.Ctx, w io.Writer) {
 	ctx.Wrap(func() {
 		fmt.Fprintf(w, "%senable: %t\n", ctx.Indent(), c.Enable)
 		fmt.Fprintf(w, "%sauthType: %s\n", ctx.Indent(), c.AuthType.orDefault())
-		fmt.Fprintf(w, "%strustedCA: %q\n", ctx.Indent(), c.TrustedCA)
+		fmt.Fprintf(w, "%scaCertFile: %q\n", ctx.Indent(), c.TrustedCA)
 		fmt.Fprintf(w, "%sclientCommonNames: %s\n", ctx.Indent(), c.ClientCommonNames)
 	})
 }
